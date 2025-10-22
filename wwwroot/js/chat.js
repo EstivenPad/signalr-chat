efe9d0826c42da090c0b12b27b64180bab4787b8
Estiven Padilla Santos <estivenps26@gmail.com>
2025-10-22 17:45:41 -0400

    adding signalR code

---
+++ wwwroot/js/chat.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("Receive Message", (user, message) => {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user}: ${message}`;
+});
+
+connection.start().then(() => {
+    document.getElementById("sendButton").disabled = false;
+}).catch((err) => {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
